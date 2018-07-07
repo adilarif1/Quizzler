43f37ab16b86eee6216305f88d33a3576af8f9c1
Adil Arif <adilarif1@icloud.com>
2018-07-07 11:28:48 +0100

    Initial commit

---
+++ Question.swift
@@ -0,0 +1,22 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Adil Arif on 03/07/2018.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Question{
+    
+    let questionText: String
+    let answer: Bool
+    
+    init(text: String, correctAnswer: Bool) {
+        
+        questionText = text
+        answer = correctAnswer
+        
+    }
+}
